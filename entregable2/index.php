56aba20863f4544cfb9a6d1bafc1ebc395ef23f6
joelcortes89 <joelandrescortesbonilla@gmail.com>
2026-03-11 07:48:20 -0500

    segundo entregables
    trata de el como sera la parte de inisio de sesion para los usuarios

---
+++ entregable2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="UTF-8">
+<title>MindFit</title>
+
+<link rel="stylesheet" href="css/style_Mindfit.css">
+
+</head>
+
+<body>
+
+<main>
+
+<h1>MindFit</h1>
+
+<p>Tu espacio de bienestar mental</p>
+
+<input type="text" placeholder="Usuario">
+<input type="password" placeholder="Contraseña">
+
+<input type="submit" value="Iniciar sesión">
+
+<input type="submit" value="Registrarse">
+
+
+</main>
+
+</body>
+</html>
